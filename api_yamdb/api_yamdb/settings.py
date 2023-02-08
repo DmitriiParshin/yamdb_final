0b0c095cf669a966b08e8401f9167fd29a7c1763
dimaska <parshindi@mail.ru>
2023-02-08 19:22:38 +0200

    fixed bugs

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs")
 
-DEBUG = False
+DEBUG = os.getenv("DEBUG", default=True)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", default="*")]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
