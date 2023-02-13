af982b5e91a7c6079362ba6a899f9a5962c0d86e
dimaska <parshindi@mail.ru>
2023-02-13 21:47:48 +0200

    fixed bugs

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,11 +3,13 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv("SECRET_KEY", "SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT")
 
 DEBUG = int(os.getenv("DEBUG", 0))
 
-ALLOWED_HOSTS = ["158.160.9.121", "localhost", "127.0.0.1", "web"]
+ALLOWED_HOSTS = os.getenv(
+    "DJANGO_ALLOWED_HOSTS",
+    ["158.160.9.121", "localhost", "127.0.0.1", "web"]).split(" ")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
